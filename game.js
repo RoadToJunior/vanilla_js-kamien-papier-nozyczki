976706f35afe30f1e39fc0251f9a4c38045d15f3
NowWeAre-Free <kontodarm@wp.pl>
2023-07-11 22:01:19 +0200

    add func start game and if statement to block empty choice

--- game.js
+++ game.js
@@ -1,6 +1,7 @@
 // Zobacz gotowy projekt: https://websamuraj.pl/examples/js/projekt7/
 
 const hands = [...document.querySelectorAll(".select img")];
+const startBtn = document.querySelector(".start");
 
 const gameSummary = {
   numbers: 0,
@@ -14,11 +15,20 @@ const game = {
   aiHand: "",
 };
 
+function aiChoice() {}
+
 function handSelection() {
   game.playerHand = this.dataset.option;
-  console.log(game.playerHand);
   hands.forEach((hand) => (hand.style.boxShadow = ""));
   this.style.boxShadow = "0 0 0 4px red";
 }
 
+function startGame() {
+  if (!game.playerHand) {
+    return alert("wybierz dłoń!");
+  }
+  game.aiHand = aiChoice();
+}
+
+startBtn.addEventListener("click", startGame);
 hands.forEach((hand) => hand.addEventListener("click", handSelection));
